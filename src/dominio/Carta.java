807571ba1527b0736f1ecc8a66443bd8f646a8f8
Vicente Guerra Veliz <lennypede01@gmail.com>
2026-07-03 01:17:57 -0400

    feat: modelo de dominio con herencia e interfaz visitable

--- src/dominio/Carta.java
+++ src/dominio/Carta.java
@@ -1,11 +1,12 @@
 package dominio;
 
+/** clase base de las cartas. guarda lo comun: nombre, rareza y tipo */
 public abstract class Carta implements Visitable {
-	
+
 	private String name;
 	private int rarity;
 	private String type;
-	
+
 	public Carta(String name, int rarity, String type) {
 		this.name = name;
 		this.rarity = rarity;
@@ -23,10 +24,14 @@ public abstract class Carta implements Visitable {
 	public String getType() {
 		return type;
 	}
-	
-	
 
-	
-	
-	
+	/** devuelve la carta como linea del txt: Nombre;Rareza;Tipo;... cada tipo agrega lo suyo */
+	public abstract String toLinea();
+
+	// asi las listas y combos de la gui muestran algo legible en vez del hash del objeto
+	@Override
+	public String toString() {
+		return name + " (" + type + ")";
+	}
+
 }
--- src/dominio/Visitable.java
+++ src/dominio/Visitable.java
@@ -4,5 +4,4 @@ import patrones.Visitor;
 public interface Visitable { // podriamos poner el accept en carta nomas pero hay puntos x diseño so ultimately we shall ball :basketball:
 
 	void accept(Visitor v);
-	
 }
